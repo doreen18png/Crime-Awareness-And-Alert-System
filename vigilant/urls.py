7e0c4c796bb280ee8f466d14daa7ebfd9b52b1fc
NSUBUGA-BRIAN <briannsubuga30@gmail.com>
2026-06-07 18:35:41 +0300

    Fix logout 405 error

--- vigilant/urls.py
+++ vigilant/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 # Customize admin panel branding
 admin.site.site_header = "Vigilant Administration"
