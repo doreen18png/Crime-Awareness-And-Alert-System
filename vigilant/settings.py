26a50ed0d4f8a188486e104f19e3a26485c9a70d
NSUBUGA-BRIAN <briannsubuga30@gmail.com>
2026-06-07 14:04:50 +0300

    Fix database configuration

--- vigilant/settings.py
+++ vigilant/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 import os
 from pathlib import Path
 from decouple import config
@@ -53,10 +54,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'vigilant.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
